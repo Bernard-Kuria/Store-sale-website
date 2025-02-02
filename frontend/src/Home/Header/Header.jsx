949e0aa8dd8e40b6fa5fbd2578ed300a03f05822
Bernard-Kuria <bernard.i.kuria@gmail.com>
2025-02-02 03:43:15 +0300

    changes on Header.jsx

--- frontend/src/Home/Header/Header.jsx
+++ frontend/src/Home/Header/Header.jsx
@@ -15,7 +15,7 @@ export default function Header({ setDisplayScroll, scrollContact }) {
   useEffect(() => {
     const fetchPassword = async () => {
       try {
-        const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:5000";
+        const apiUrl = import.meta.env.VITE_API_URL;
         const response = await fetch(`${apiUrl}/admin/password`);
         if (response.ok) {
           setPasswordFetched(true); // Set fetched status
