ffd91469c1d8db7ed200f4a97b7caeb7a4ef3b78
Bernard-Kuria <bernard.i.kuria@gmail.com>
2025-02-03 17:22:37 +0300

    reverting addItem component code

--- backend/server.js
+++ backend/server.js
@@ -172,10 +172,9 @@ app.get("/store", async (req, res) => {
   }
 });
 
+// Add a new shoe
 app.post("/store", upload.single("image"), async (req, res) => {
-  console.log("Received body:", req.body);
-  console.log("Received file:", req.file);
-
+  console.log("Received file:", req.file); // Add this for debugging
   const { productName, price, stock } = req.body;
   const image = req.file ? req.file.filename : null;
 
