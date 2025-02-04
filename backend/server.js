2b9c743b994b1b6fe06965e8decbc174f215650b
Bernard-Kuria <bernard.i.kuria@gmail.com>
2025-02-04 18:36:37 +0300

    Ensure Express Serves Static Files

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,7 @@ app.use(bodyParser.json());
 app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use((err, req, res, next) => {
   console.error("Internal Server Error:", err);
